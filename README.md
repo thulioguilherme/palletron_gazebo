71b5d154061923edeb816efd8ea94d09404725c5
thulioguilherme <thulioguilherme@hotmail.com>
2023-05-14 14:42:25 -0300

    reorganized urdf folder, added README and optimized launch

---
+++ README.md
@@ -0,0 +1,31 @@
+# The Palletron Gazebo repository
+
+This repository provides the ROS package to simulate the Palletron robot on Gazebo.
+
+## Getting started
+
+* On your workspace, clone the [AWS RoboMaker Small Warehouse World](https://github.com/aws-robotics/aws-robomaker-small-warehouse-world) from the `ros2` branch.
+  ```
+  cd ~/<your_workspace>/src
+  git clone https://github.com/aws-robotics/aws-robomaker-small-warehouse-world.git
+  git checkout ros2
+  ```
+* Clone this repository.
+  ```
+  cd ~/<your_workspace>/src
+  git clone https://github.com/thulioguilherme/palletron_gazebo.git
+  ```
+* Install dependencies.
+  ```
+  cd ~/<your_workspace>
+  rosdep install --from-paths src --ignore-src -r -y
+  ```
+* Build it!
+  ```
+  colcon build
+  ```
+* Source the `setup.bash` from the `install` folder, and then launch a simulation.
+  ```
+  source install/setup.bash
+  ros2 launch palletron_gazebo palletron_simulation.launch.py
+  ```
--- launch/palletron_simulation.launch.py
+++ launch/palletron_simulation.launch.py
@@ -3,13 +3,13 @@ import os
 from ament_index_python.packages import get_package_share_directory
 
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, ExecuteProcess, IncludeLaunchDescription
-from launch.conditions import IfCondition , UnlessCondition
+from launch.actions import DeclareLaunchArgument, TimerAction, ExecuteProcess, IncludeLaunchDescription, RegisterEventHandler
+from launch.conditions import IfCondition
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import LaunchConfiguration, PythonExpression, Command
 from launch_ros.actions import Node
+from launch.event_handlers import OnProcessStart
 from launch_ros.parameter_descriptions import ParameterValue
-
 
 def generate_launch_description():
     # Get the launch directory
@@ -18,67 +18,47 @@ def generate_launch_description():
 
     # Get the AWS Robomaker small warehouse directory
     aws_robomaker_small_warehouse_dir = get_package_share_directory('aws_robomaker_small_warehouse_world')
+    
+    # Get the Gazebo directory
     gazebo_ros = get_package_share_directory('gazebo_ros')
 
     # Launch configuration variables
-    namespace = LaunchConfiguration('namespace')
-    use_sim_time = LaunchConfiguration('use_sim_time')
-    use_simulator = LaunchConfiguration('use_simulator')
-    use_robot_state_pub = LaunchConfiguration('use_robot_state_pub')
     headless = LaunchConfiguration('headless')
     world = LaunchConfiguration('world')
+    robot_name = LaunchConfiguration('robot_name')
+    robot_urdf = LaunchConfiguration('robot_urdf')
     pose = {'x': LaunchConfiguration('x_pose', default = '0.0'),
             'y': LaunchConfiguration('y_pose', default = '0.0'),
-            'z': LaunchConfiguration('z_pose', default = '0.32'),
+            'z': LaunchConfiguration('z_pose', default = '0.05'),
             'R': LaunchConfiguration('roll', default = '0.00'),
             'P': LaunchConfiguration('pitch', default = '0.00'),
             'Y': LaunchConfiguration('yaw', default = '0.00')}
-    robot_name = LaunchConfiguration('robot_name')
-    robot_xacro = LaunchConfiguration('robot_xacro')
-    remappings = [('/tf', 'tf'),
-                  ('/tf_static', 'tf_static')]
 
     # Declare the launch arguments
-    declare_namespace_cmd = DeclareLaunchArgument(
-        'namespace',
-        default_value = '',
-        description = 'Top-level namespace')
-
-    declare_use_sim_time_cmd = DeclareLaunchArgument(
-        'use_sim_time',
-        default_value = 'true',
-        description = 'Use simulation (Gazebo) clock if true')
-
-    declare_use_simulator_cmd = DeclareLaunchArgument(
-        'use_simulator',
-        default_value = 'True',
-        description = 'Whether to start the simulator')
-
-    declare_use_robot_state_pub_cmd = DeclareLaunchArgument(
-        'use_robot_state_pub',
-        default_value = 'True',
-        description = 'Whether to start the robot state publisher')
+    declare_headless_cmd = DeclareLaunchArgument(
+        'headless',
+        default_value = 'False',
+        description = 'Whether to execute gzclient)'
+    )
 
     declare_world_cmd = DeclareLaunchArgument(
         'world',
         default_value = os.path.join(aws_robomaker_small_warehouse_dir, 'worlds', 'no_roof_small_warehouse', 'no_roof_small_warehouse.world'),
-        description = 'Full path to world model file to load')
+        description = 'Full path to world model file to load'
+    )
 
     declare_robot_name_cmd = DeclareLaunchArgument(
         'robot_name',
         default_value = 'palletron',
-        description = 'Name of the robot')
+        description = 'Name of the robot'
+    )
 
-    declare_robot_xacro_cmd = DeclareLaunchArgument(
-        'robot_xacro',
-        default_value = os.path.join(palletron_gazebo_dir, 'urdf', 'palletron.gazebo.xacro'),
-        description = 'Full path to robot xacro file to spawn the robot in gazebo')
+    declare_robot_urdf_cmd = DeclareLaunchArgument(
+        'robot_urdf',
+        default_value = os.path.join(palletron_gazebo_dir, 'urdf', 'palletron.urdf.xacro'),
+        description = 'Full path to robot urdf file to spawn the robot in gazebo'
+    )
     
-    declare_simulator_cmd = DeclareLaunchArgument(
-        'headless',
-        default_value = 'False',
-        description = 'Whether to execute gzclient)')
-
     # Specify the actions
     start_gazebo_server_cmd = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
@@ -88,19 +68,20 @@ def generate_launch_description():
     start_gazebo_client_cmd = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
             os.path.join(gazebo_ros, 'launch', 'gzclient.launch.py')),
-        condition = IfCondition(PythonExpression([use_simulator, ' and not ', headless]))
+        condition = IfCondition(PythonExpression(['not ', headless]))
     )
 
-    urdf = os.path.join(palletron_gazebo_dir, 'urdf', 'palletron.gazebo.xacro')
-    with open(urdf, 'r') as infp:
-        robot_description = infp.read()
-
     start_robot_state_publisher_cmd = Node(
-        condition = IfCondition(use_robot_state_pub),
         package = 'robot_state_publisher',
         executable = 'robot_state_publisher',
         output = 'screen',
-        parameters = [{'robot_description': ParameterValue(Command(['xacro ', urdf]), value_type = str)}])
+        parameters = [{'robot_description': ParameterValue(Command(['xacro ', robot_urdf]), value_type = str)}]
+    )
+
+    start_joint_state_publisher_cmd = Node(
+        package = "joint_state_publisher",
+        executable = "joint_state_publisher"
+    )
 
     start_gazebo_spawner_cmd = Node(
         package = 'gazebo_ros',
@@ -110,65 +91,40 @@ def generate_launch_description():
             '-entity', robot_name,
             '-topic', '/robot_description',
             '-x', pose['x'], '-y', pose['y'], '-z', pose['z'],
-            '-R', pose['R'], '-P', pose['P'], '-Y', pose['Y']])
+            '-R', pose['R'], '-P', pose['P'], '-Y', pose['Y']]
+    )
     
     start_rviz_cmd = Node(
-       package='rviz2',
-       namespace= namespace,
-       executable='rviz2',
-        arguments=[
+       package = 'rviz2',
+       executable = 'rviz2',
+        arguments = [
            '-d' + os.path.join(get_package_share_directory('palletron_gazebo'), 
-           'rviz', 'palletron_config.rviz')]
-            
+           'rviz', 'palletron.rviz')]
     )
 
-    # start_diff_drive_spawner_cmd = Node(
-    #     package="controller_manager",
-    #     executable="spawner",       
-    #     arguments=["diff_cont"],
-    # )
-
-    # start_joint_broad_spawner_cmd = Node(
-    #     package="controller_manager",
-    #     executable="spawner",     
-    #     arguments=["joint_broad"],
-    # )
-
-#     declare_joint_state_publisher_cmd = Node(
-#    	    package="joint_state_publisher",
-#    	    executable="joint_state_publisher", 	 
-#    	    condition=UnlessCondition(joint_gui)
-#    )
-    
-#     declare_joint_state_publisher_gui_cmd = Node(
-#    	    package="joint_state_publisher_gui",
-#    	    executable="joint_state_publisher_gui",   
-#    	    condition=IfCondition(joint_gui)
-#    )
+    delay_rviz_after_joint_state_publisher_cmd = RegisterEventHandler(
+        event_handler = OnProcessStart(
+            target_action = start_joint_state_publisher_cmd,
+            on_start = [
+                TimerAction(period = 5.0, actions = [start_rviz_cmd])],
+        )
+    )
 
     # Create the launch description and populate
     ld = LaunchDescription()
 
-    # Declare the launch options
-    ld.add_action(declare_namespace_cmd)
-
-    ld.add_action(declare_use_simulator_cmd)
-    ld.add_action(declare_use_robot_state_pub_cmd)
-    ld.add_action(declare_simulator_cmd)
+    # Parameters
+    ld.add_action(declare_headless_cmd)
     ld.add_action(declare_world_cmd)
     ld.add_action(declare_robot_name_cmd)
-    ld.add_action(declare_robot_xacro_cmd)
-    # ld.add_action(declare_joint_state_publisher_cmd)
-    # ld.add_action(declare_joint_state_publisher_gui_cmd)
-    #ld.add_action(declare_joint_gui_cmd)
-
-    # Add any conditioned actions
+    ld.add_action(declare_robot_urdf_cmd)
+    
+    # Actions
     ld.add_action(start_gazebo_server_cmd)
     ld.add_action(start_gazebo_client_cmd)
     ld.add_action(start_gazebo_spawner_cmd)
-    ld.add_action(start_rviz_cmd)
-    # Add the actions to launch all of the navigation nodes
     ld.add_action(start_robot_state_publisher_cmd)
-    # ld.add_action(start_joint_broad_spawner_cmd)
-    # ld.add_action(start_diff_drive_spawner_cmd)
+    ld.add_action(start_joint_state_publisher_cmd)
+    ld.add_action(delay_rviz_after_joint_state_publisher_cmd)
+
     return ld
