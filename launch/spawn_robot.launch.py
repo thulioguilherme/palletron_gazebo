c4e908b6148bde71ec2320361e8f60a9685fa8ee
Thulio <thulioguilherme@hotmail.com>
2024-10-30 17:57:36 -0300

    removed the multi-robot spawner

--- launch/spawn_robot.launch.py
+++ launch/spawn_robot.launch.py
@@ -9,6 +9,22 @@ from launch_ros.actions import Node
 from launch_ros.parameter_descriptions import ParameterValue
 from launch_ros.substitutions import FindPackageShare
 
+import yaml
+
+
+def validate_pose_file(pose_file_path, robot_name):
+    try:
+        with open(pose_file_path) as file:
+            pose_dict = yaml.load(file, Loader=yaml.loader.SafeLoader)
+    except Exception as err:
+        raise(err) # This will crash the launcher on purpose to stop it
+
+    # TODO(thulioguilherme): Also validate the YAML data structure
+    if robot_name in pose_dict:
+        return pose_dict[robot_name]
+
+    raise Exception('The robot name is not in the pose file.')
+
 
 def launch_setup(context: LaunchContext):
     # Initialize arguments
@@ -16,13 +32,11 @@ def launch_setup(context: LaunchContext):
     robot_name = LaunchConfiguration('robot_name')
     robot_urdf = LaunchConfiguration('robot_urdf')
     robot_optional_properties = LaunchConfiguration('robot_optional_properties')
-    pose = {'x': LaunchConfiguration('x_pose'),
-            'y': LaunchConfiguration('y_pose'),
-            'z': LaunchConfiguration('z_pose'),
-            'R': LaunchConfiguration('roll'),
-            'P': LaunchConfiguration('pitch'),
-            'Y': LaunchConfiguration('yaw')}
+    pose_yaml_file = LaunchConfiguration('pose_yaml_file')
     use_sim_time = LaunchConfiguration('use_sim_time')
+
+    # Validate pose params file
+    robot_spawn_pose = validate_pose_file(pose_yaml_file.perform(context), namespace.perform(context))
 
     # Put namespace on the robot description topic name
     robot_description_topic = namespace.perform(context) + '/robot_description'
@@ -57,12 +71,10 @@ def launch_setup(context: LaunchContext):
             '-entity', robot_name,
             '-topic', TextSubstitution(text=robot_description_topic),
             '-robot_namespace', namespace,
-            '-x', pose['x'],
-            '-y', pose['y'],
-            '-z', pose['z'],
-            '-R', pose['R'],
-            '-P', pose['P'],
-            '-Y', pose['Y']])
+            '-x', TextSubstitution(text=str(robot_spawn_pose['x'])),
+            '-y', TextSubstitution(text=str(robot_spawn_pose['y'])),
+            '-z', TextSubstitution(text=str(robot_spawn_pose['z'])),
+            '-Y', TextSubstitution(text=str(robot_spawn_pose['yaw']))])
 
     return [robot_state_publisher_node, joint_state_publisher_node, spawn_robot_node]
 
@@ -74,13 +86,13 @@ def generate_launch_description():
     declared_arguments.append(
         DeclareLaunchArgument(
             'namespace',
-            default_value='palletron1',
+            default_value='ugv1',
             description='Top-level namespace.'))
 
     declared_arguments.append(
         DeclareLaunchArgument(
             'robot_name',
-            default_value='palletron1',
+            default_value='palletron',
             description='Name of the robot.'))
 
     declared_arguments.append(
@@ -99,44 +111,15 @@ def generate_launch_description():
 
     declared_arguments.append(
         DeclareLaunchArgument(
+            'pose_yaml_file',
+            default_value=PathJoinSubstitution(
+                [FindPackageShare('palletron_gazebo'), 'params', 'spawn', 'pose.yaml']),
+            description='Full path to the file with the pose to spawn the robot.'))
+
+    declared_arguments.append(
+        DeclareLaunchArgument(
             'use_sim_time',
             default_value='True',
             description='Whether use simulation (Gazebo) clock.'))
-
-    declared_arguments.append(
-        DeclareLaunchArgument(
-            'x_pose',
-            default_value='0.00',
-            description='The x-component of the initial position (meters).'))
-
-    declared_arguments.append(
-        DeclareLaunchArgument(
-            'y_pose',
-            default_value='-1.00',
-            description='The y-component of the initial position (meters).'))
-
-    declared_arguments.append(
-        DeclareLaunchArgument(
-            'z_pose',
-            default_value='0.05',
-            description='The z-component of the initial position (meters).'))
-
-    declared_arguments.append(
-        DeclareLaunchArgument(
-            'roll',
-            default_value='0.00',
-            description='The roll angle of the initial position (radians).'))
-
-    declared_arguments.append(
-        DeclareLaunchArgument(
-            'pitch',
-            default_value='0.00',
-            description='The pitch angle of the initial position (radians).'))
-
-    declared_arguments.append(
-        DeclareLaunchArgument(
-            'yaw',
-            default_value='0.00',
-            description='The yaw angle of the initial position (radians).'))
 
     return LaunchDescription(declared_arguments + [OpaqueFunction(function=launch_setup)])
